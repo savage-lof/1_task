50a0b4746b9014d627d9d61ed8effa492165d7e3
savage-lof <lomova20042004@ya.ru>
2021-02-26 18:18:55 +0300

    qwe2

--- server.py
+++ server.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/index/<name>')
-def index(name):
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title=name, username=user)
+@app.route('/training/<profs>')
+def index(profs):
+    return render_template('index.html', prof=profs)
 
 
 if __name__ == '__main__':
