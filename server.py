7ac45e938ac665653819cc73da48e8ce8bd92dd2
yulia <lomova20042004@ya.ru>
2021-03-06 23:21:21 +0300

    2 задание

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index(name):
     return render_template('index.html', title=name)
 
+@app.route('/training/<profs>')
+def profession(profs):
+    return render_template('index.html', prof=profs)
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
