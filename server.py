9a646bdc38a81f16574865d5866c78e6db494279
yulia <lomova20042004@ya.ru>
2021-03-08 15:52:53 +0300

    3 задание

--- server.py
+++ server.py
@@ -10,5 +10,9 @@ def index(name):
 def profession(profs):
     return render_template('index.html', prof=profs)
 
+@app.route('/list_prof/<list>')
+def list_of_professions(list):
+    return render_template('index.html', list=list)
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
